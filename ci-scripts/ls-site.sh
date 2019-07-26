c687eb89e44918010aa512f626d0fe12c9b3fe9f
gz-c <token@protonmail.com>
2019-07-27 00:11:36 +0800

    Update travis

--- ci-scripts/ls-site.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "s3cmd --access_key=${AWS_ACCESS_KEY_ID} --secret_key=${AWS_SECRET_ACCESS_KEY} --region=ap-southeast-1 ls s3://www.skycoin.net/docs/"
-s3cmd --access_key="${AWS_ACCESS_KEY_ID}" --secret_key="${AWS_SECRET_ACCESS_KEY}" --region=ap-southeast-1 ls s3://www.skycoin.net/docs/
